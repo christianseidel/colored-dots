c9ea9216f19c3b35ee2605934f32c586833ee3c4
Christian Seidel <github@parameter5.de>
2022-05-18 16:30:44 +0200

    restored

--- backend/src/main/java/project/DotsService.java
+++ backend/src/main/java/project/DotsService.java
@@ -16,4 +16,70 @@ public class DotsService {
         return listOfColors;
     }
 
+    // Using the Netscape Color Scheme:
+    private String getColor() {
+        int x = randomNumber(12);
+        String color = "";
+        switch(x) {
+            case 1:
+                color = "#FF0000";
+                break;
+            case 2:
+                color = "#00FF00";
+                break;
+            case 3:
+                color = "#0000FF";
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+                color = getRandomColor();
+        }
+        return color;
+    }
+
+    private String getRandomColor() {
+        String color = "#";
+        for (int i = 0; i <= 2; i++) {
+            color = color + getColorByte();
+        }
+        return color;
+    }
+
+    private String getColorByte() {
+        String hex = "";
+        int c = randomNumber(6);
+        switch(c) {
+            case 1:
+                hex = "00";
+                break;
+            case 2:
+                hex = "33";
+                break;
+            case 3:
+                hex = "66";
+                break;
+            case 4:
+                hex = "99";
+                break;
+            case 5:
+                hex = "CC";
+                break;
+            case 6:
+                hex = "FF";
+                break;
+        }
+        return hex;
+    }
+
+    private int randomNumber(int n) {
+        int x = (int)((Math.random() * n) + 1);
+        return x;
+    }
 }
